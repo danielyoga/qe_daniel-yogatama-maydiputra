8465fcd1fa1efaf90cdc3b82c2f061b5321d4ac2
unknown <danielyogatama.dy@gmail.com>
2022-03-03 15:56:25 +0700

    done

---
+++ 10_Introduction to Basic Programming/praktikum/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        float alas = 20;
+        float tinggi = 25;
+        float luas = 0.5f * alas * tinggi;
+        System.out.printf("%.2f \n", luas);
+    }
+}
